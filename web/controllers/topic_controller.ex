5b58fea0a2e07b2a4edaa3746f9052a34da75176
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2020-07-24 17:05:53 -0300

    added update and delete topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,14 +3,64 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
-  def new(conn, params) do
-    changeset = Topic.changeset(%Topic{}, %{})
+  def index(conn, _params) do
+
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{})
     render conn, "new.html", changeset: changeset
   end
 
-  def create(conn, %{"topic" => topic }) do
+  def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
-    render conn, "new.html", changeset: changeset
+
+    case Repo.insert(changeset) do
+      {:ok, _topic} -> 
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"topic" => topic_params, "id" => topic_id}) do
+
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic, topic_params)
+
+    case Repo.update(changeset) do
+      {:ok, _struct} -> 
+        conn
+        |> put_flash(:info, "Topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} -> 
+        conn
+        |> put_flash(:error, "Failed to update")
+        |> render("edit.html", changeset: changeset, topic: topic)
+    end
+  end
+
+  def delete(conn, %{"id" => topic_id}) do
+    
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect to: topic_path(conn, :index)
+
   end
 
 end
